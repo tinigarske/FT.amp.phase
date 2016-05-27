79aeac261431d60aaa0bc69e53d7ef9c84824b29
tinigarske <t.garske@imperial.ac.uk>
2016-05-27 10:47:01 +0100

    adding function to convert all modes from Re/Im to amp/phase

---
+++ R/Re.Im.to.amp.phase.all.modes.R
@@ -0,0 +1,35 @@
+##' Calculate the amplitude and phase of several modes of Fourier
+##' transforms given the real and imaginary parts of the modes.
+##'
+##' .. content for \details{} ..
+##' @title Amplitude and pase of the full Fourier transform
+##' @param FT matrix or dataframe of the modes of interest of several
+##' Fourier transforms. Each individual FT is given in a row, and
+##' columns with individual modes are expected to be named "Rex" and
+##' "Imx", for mode x where x is an integer (although it does not need
+##' to be tied to the frequency of the mode).
+##' @param n.time.points Number of time points for which the Fourier
+##' transform was calculated. Default value is 512 ( = 8 years * 64
+##' points per year).
+##' @return A matrix with amplitude and phase columns for each mode within the input data FT
+##' @author Tini
+##' @export
+Re.Im.to.amp.phase.all.modes = function(FT, n.time.points = 512) {
+  re.cols = grep("^Re", names(FT))
+  re.modes = as.numeric(gsub("Re", "", names(FT)[re.cols]))
+  im.cols = grep("^Im", names(FT))
+  im.modes = as.numeric(gsub("Im", "", names(FT)[im.cols]))
+
+  ## making sure that re and im columns are ordered in the same way
+  ## (which they should be anyway):
+  im.cols = im.cols[match(re.modes, im.modes)]
+  im.modes = im.modes[match(re.modes, im.modes)]
+  
+  amp.phase = NULL
+  for(i in 1:length(re.cols)) {
+    amp.phase = cbind(amp.phase, calc.amp.phase(Re = FT[, re.cols[i]], Im = FT[, im.cols[i]], nyears = nyears, ppyear = ppyear))
+    colnames(amp.phase)[ncol(amp.phase) + c(-1,0)] = paste0(colnames(amp.phase)[ncol(amp.phase) + c(-1,0)], re.modes[i])
+  }
+
+  return(amp.phase)
+}
